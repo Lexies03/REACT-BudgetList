dcc5124db6182faa75c086c70dbfdacc9a60ff04
Alissa Mae Celeste <alissamaeceleste@gmail.com>
2023-08-16 13:18:29 +0800

    Resolving issues on deployment: Changed bg-logo.png into bg-Logo.png on Registration Page.

--- p3-react-app/src/pages/RegistrationForm.jsx
+++ p3-react-app/src/pages/RegistrationForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Logo from "../Assets/bg-logo.png";
+import Logo from "../Assets/bg-Logo.png";
 import { Link } from "react-router-dom";
 
 const RegistrationForm = () => {
